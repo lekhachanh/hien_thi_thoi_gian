575362512117b4d76793e2434ac2a6ccabb05e9c
lekhachanh <lekhachanh123@gmail.com>
2019-07-04 10:56:17 +0700

    fix

---
+++ src/systemTime.java
@@ -0,0 +1,9 @@
+import java.time.Clock;
+import java.util.Date;
+
+public class systemTime {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println("Date now: "+ now);
+    }
+}
